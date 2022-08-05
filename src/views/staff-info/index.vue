dac7b836f6dd7444b48a8e28d396e2ddc162985b
zuojuntao <2275958074@qq.com>
2022-08-05 11:33:31 +0800

    员工信息界面

--- src/views/staff-info/index.vue
+++ src/views/staff-info/index.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="app-container">
-    <el-form label-position="right" label-width="80px">
+    <el-form ref="staffInfoForm" :model="form" label-position="right" label-width="100px" :rules="rules">
       <div class="staff-info wrap">
         <div class="staff-info-module">
           <div class="staff-info-module-title">基本信息</div>
           <el-divider />
           <div class="staff-info-form">
-            <el-row>
+            <el-row :gutter="100">
               <el-col :span="12">
                 <el-form-item label="姓名:" prop="name">
                   <el-input
-                    v-model="formData.name"
+                    v-model="form.name"
                     placeholder="请输入姓名"
                     clearable
                   />
@@ -19,7 +19,7 @@
               <el-col :span="12">
                 <!-- 性别（0男，1女） -->
                 <el-form-item label="性别:" prop="sex">
-                  <el-radio-group v-model="formData.sex">
+                  <el-radio-group v-model="form.sex">
                     <el-radio
                       v-for="item in sexOptions"
                       :key="'sex' + item.value"
@@ -29,11 +29,11 @@
                 </el-form-item>
               </el-col>
             </el-row>
-            <el-row>
+            <el-row :gutter="100">
               <el-col :span="12">
                 <el-form-item label="出生日期:" prop="birthday">
                   <el-date-picker
-                    v-model="formData.birthday"
+                    v-model="form.birthday"
                     format="yyyy-MM-dd"
                     class="input-width"
                     placeholder="请选择出生日期"
@@ -44,7 +44,7 @@
               <el-col :span="12">
                 <!-- 在职状态（0：试用员工 1：正式员工 2：离职员工） -->
                 <el-form-item label="在职状态:" prop="jobStatus">
-                  <el-radio-group v-model="formData.jobStatus">
+                  <el-radio-group v-model="form.jobStatus">
                     <el-radio
                       v-for="item in jobStatusOptions"
                       :key="'jobStatus' + item.value"
@@ -54,11 +54,11 @@
                 </el-form-item>
               </el-col>
             </el-row>
-            <el-row>
+            <el-row :gutter="100">
               <el-col :span="12">
                 <el-form-item label="身份证号码:" prop="idCard">
                   <el-input
-                    v-model="formData.idCard"
+                    v-model="form.idCard"
                     placeholder="请输入身份证号码"
                     clearable
                   />
@@ -67,18 +67,18 @@
               <el-col :span="12">
                 <el-form-item label="联系电话:" prop="mobile">
                   <el-input
-                    v-model="formData.mobile"
+                    v-model="form.mobile"
                     placeholder="请输入联系电话"
                     clearable
                   />
                 </el-form-item>
               </el-col>
             </el-row>
-            <el-row>
+            <el-row :gutter="100">
               <el-col :span="12">
                 <el-form-item label="现住址:" prop="address">
                   <el-input
-                    v-model="formData.address"
+                    v-model="form.address"
                     placeholder="请输入现住址"
                     clearable
                   />
@@ -87,25 +87,25 @@
               <el-col :span="12">
                 <el-form-item label="户籍地址:" prop="sourceAddress">
                   <el-input
-                    v-model="formData.sourceAddress"
+                    v-model="form.sourceAddress"
                     placeholder="请输入户籍地址"
                     clearable
                   />
                 </el-form-item>
               </el-col>
             </el-row>
-            <el-row>
+            <el-row :gutter="100">
               <el-col :span="12">
                 <el-form-item label="婚姻状况:" prop="maritalStatus">
                   <el-select
-                    v-model="formData.maritalStatus"
+                    v-model="form.maritalStatus"
                     placeholder="请选择婚姻状况"
                     clearable
                     class="input-width"
                   >
                     <el-option
-                      v-for="(item) in maritalStatusOptions"
-                      :key="'maritalStatus'+item.value"
+                      v-for="item in maritalStatusOptions"
+                      :key="'maritalStatus' + item.value"
                       :label="item.label"
                       :value="item.value"
                     />
@@ -116,14 +116,14 @@
                 <!-- TODO -->
                 <el-form-item label="政治面貌:" prop="projectId2">
                   <el-select
-                    v-model="formData.projectId2"
+                    v-model="form.projectId2"
                     placeholder="请选择政治面貌"
                     clearable
                     class="input-width"
                   >
                     <el-option
-                      v-for="(item,index) in politicsOptions"
-                      :key="'projectId2'+index+item.projectId"
+                      v-for="(item, index) in politicsOptions"
+                      :key="'projectId2' + index + item.projectId"
                       :label="item.projectName"
                       :value="item.projectId"
                     />
@@ -131,127 +131,11 @@
                 </el-form-item>
               </el-col>
             </el-row>
-            <el-row>
-              <el-col :span="12">
-                <el-form-item label="学历:" prop="education">
-                  <el-select
-                    v-model="formData.education"
-                    placeholder="请选择学历"
-                    clearable
-                    class="input-width"
-                  >
-                    <el-option
-                      v-for="(item) in educationOptions"
-                      :key="'education'+item.value"
-                      :label="item.label"
-                      :value="item.value"
-                    />
-                  </el-select>
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="毕业时间:" prop="graduateDate">
-                  <el-date-picker
-                    v-model="formData.graduateDate"
-                    format="yyyy-MM-dd"
-                    class="input-width"
-                    placeholder="请选择毕业时间"
-                    clearable
-                  />
-                </el-form-item>
-              </el-col>
-            </el-row>
-            <el-row>
-              <el-col :span="12">
-                <el-form-item label="毕业学校:" prop="graduateSchool">
-                  <el-input
-                    v-model="formData.graduateSchool"
-                    placeholder="请输入毕业学校"
-                    clearable
-                  />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="在校专业:" prop="speciality">
-                  <el-input
-                    v-model="formData.speciality"
-                    placeholder="请输入在校专业"
-                    clearable
-                  />
-                </el-form-item>
-              </el-col>
-            </el-row>
-            <el-row>
-              <el-col :span="12">
-                <el-form-item label="入职时间:" prop="entryDate">
-                  <el-date-picker
-                    v-model="formData.entryDate"
-                    format="yyyy-MM-dd"
-                    class="input-width"
-                    placeholder="请选择入职时间"
-                    clearable
-                  />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="入职部门:" prop="uemDeptId">
-                  <el-select
-                    v-model="formData.uemDeptId"
-                    placeholder="请选择入职部门"
-                    clearable
-                    class="input-width"
-                  >
-                    <el-option
-                      v-for="(item) in deptOptions"
-                      :key="'uemDeptId'+item.uemDeptId"
-                      :label="item.deptName"
-                      :value="item.uemDeptId"
-                    />
-                  </el-select>
-                </el-form-item>
-              </el-col>
-            </el-row>
-            <el-row>
-              <el-col :span="12">
-                <el-form-item label="入职岗位:" prop="staffDutyCode">
-                  <el-select
-                    v-model="formData.staffDutyCode"
-                    placeholder="请选择入职岗位"
-                    clearable
-                    class="input-width"
-                  >
-                    <el-option
-                      v-for="(item,index) in staffDutyOptions"
-                      :key="'staffDutyCode'+index+item.staffDutyCode"
-                      :label="item.staffDuty"
-                      :value="item.staffDutyCode"
-                    />
-                  </el-select>
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="岗位职称:" prop="technicalTitleId">
-                  <el-select
-                    v-model="formData.technicalTitleId"
-                    placeholder="请选择岗位职称"
-                    clearable
-                    class="input-width"
-                  >
-                    <el-option
-                      v-for="(item) in technicalOptions"
-                      :key="'technicalTitleId'+item.technicalTitleId"
-                      :label="item.technicalName"
-                      :value="item.technicalTitleId"
-                    />
-                  </el-select>
-                </el-form-item>
-              </el-col>
-            </el-row>
-            <el-row>
+            <el-row :gutter="100">
               <el-col :span="12">
                 <el-form-item label="电子邮箱:" prop="email">
                   <el-input
-                    v-model="formData.email"
+                    v-model="form.email"
                     placeholder="请输入电子邮箱"
                     clearable
                   />
@@ -260,7 +144,7 @@
               <el-col :span="12">
                 <el-form-item label="工作年限:" prop="seniority">
                   <el-input
-                    v-model="formData.seniority"
+                    v-model="form.seniority"
                     placeholder="请输入工作年限"
                     clearable
                   />
@@ -272,10 +156,127 @@
         <div class="staff-info-module">
           <div class="staff-info-module-title">毕业学校信息</div>
           <el-divider />
+          <el-row :gutter="100">
+            <el-col :span="12">
+              <el-form-item label="学历:" prop="education">
+                <el-select
+                  v-model="form.education"
+                  placeholder="请选择学历"
+                  clearable
+                  class="input-width"
+                >
+                  <el-option
+                    v-for="item in educationOptions"
+                    :key="'education' + item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="毕业时间:" prop="graduateDate">
+                <el-date-picker
+                  v-model="form.graduateDate"
+                  format="yyyy-MM-dd"
+                  class="input-width"
+                  placeholder="请选择毕业时间"
+                  clearable
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row :gutter="100">
+            <el-col :span="12">
+              <el-form-item label="毕业学校:" prop="graduateSchool">
+                <el-input
+                  v-model="form.graduateSchool"
+                  placeholder="请输入毕业学校"
+                  clearable
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="在校专业:" prop="speciality">
+                <el-input
+                  v-model="form.speciality"
+                  placeholder="请输入在校专业"
+                  clearable
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
         </div>
         <div class="staff-info-module">
           <div class="staff-info-module-title">入职信息</div>
           <el-divider />
+          <el-row :gutter="100">
+            <el-col :span="12">
+              <el-form-item label="入职时间:" prop="entryDate">
+                <el-date-picker
+                  v-model="form.entryDate"
+                  format="yyyy-MM-dd"
+                  value-format="yyyy-MM-dd"
+                  class="input-width"
+                  placeholder="请选择入职时间"
+                  clearable
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="岗位职称:" prop="technicalTitleId">
+                <el-select
+                  v-model="form.technicalTitleId"
+                  placeholder="请选择岗位职称"
+                  clearable
+                  class="input-width"
+                >
+                  <el-option
+                    v-for="item in technicalOptions"
+                    :key="'technicalTitleId' + item.technicalTitleId"
+                    :label="item.technicalName"
+                    :value="item.technicalTitleId"
+                  />
+                </el-select>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row :gutter="100">
+            <el-col :span="12">
+              <el-form-item label="入职岗位:" prop="staffDutyCode">
+                <StaffDuty v-model="form.staffDutyCode" class="input-width" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="个人简历:">
+                <el-button type="text">上传</el-button>
+              </el-form-item>
+            </el-col>
+          </el-row>
+
+          <el-row :gutter="100">
+            <el-col :span="12">
+              <!-- TODO 联想控件 -->
+              <el-form-item label="归属项目:" prop="projectId">
+                <el-select
+                  v-model="form.projectId"
+                  placeholder="请选择归属项目"
+                  clearable
+                  class="input-width"
+                >
+                  <el-option
+                    v-for="(item, index) in projectTypeOptions"
+                    :key="'projectId' + index + item.projectId"
+                    :label="item.projectName"
+                    :value="item.projectId"
+                  />
+                </el-select>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </div>
+        <div class="operate-button">
+          <el-button type="primary" @click="handleSave">保存</el-button>
         </div>
       </div>
     </el-form>
@@ -314,11 +315,69 @@ export default {
       sexOptions: this.$dict.getDictOptions('SEX'), // 性别
       maritalStatusOptions: this.$dict.getDictOptions('MARITAL_STATUS'), // 婚姻状况
       educationOptions: this.$dict.getDictOptions('EDUCATION'), // 学历
+      jobStatusOptions: this.$dict.getDictOptions('JOB_STATUS'), // 在职状态
+
       technicalOptions: [], // 岗位职称
       staffDutyOptions: [], // 入职岗位
       projectTypeOptions: [], // 归属项目
-      politicsOptions: []
-    };
+      politicsOptions: [], // 政治面貌
+
+      rules: {
+        name: [{ required: true, message: '请输入名字', trigger: 'change' }],
+        sex: [{ required: true, message: '请选择性别', trigger: 'change' }],
+        birthday: [{ required: true, message: '请选择出生日期', trigger: 'change' }],
+        jobStatus: [{ required: true, message: '请选择在职状态', trigger: 'change' }],
+        idCard: [{ validator: this.validateIdCard, trigger: 'blur' }],
+        mobile: [{ validator: this.validateMobile, trigger: 'blur' }],
+        address: [{ required: true, message: '请输入现住址', trigger: 'change' }],
+        // sourceAddress: [{ required: true, message: "请输入户籍地址", trigger: change }],
+        email: [{ validator: this.validateEmail, trigger: 'blur' }],
+        seniority: [{ required: true, message: '请输入工作年限', trigger: 'change' }],
+        education: [{ required: true, message: '请选择学历', trigger: 'change' }],
+        graduateDate: [{ required: true, message: '请选择毕业时间', trigger: 'change' }],
+        graduateSchool: [{ required: true, message: '请输入毕业学校', trigger: 'change' }],
+        speciality: [{ required: true, message: '请输入在校专业', trigger: 'change' }],
+        entryDate: [{ required: true, message: '请选择入职时间', trigger: 'change' }],
+        technicalTitleId: [{ required: true, message: '请选择岗位职称', trigger: 'change' }],
+        staffDutyCode: [{ required: true, message: '请选择入职岗位', trigger: 'change' }],
+        projectId: [{ required: true, message: '请选择归属项目', trigger: 'change' }]
+      }
+
+    }
+  },
+  methods: {
+    handleSave() {
+      // this.$refs.staffInfoForm.validate((valid) => {
+      //   if (valid) {
+      //   }
+      // })
+    },
+    validateEmail(rule, value, callback) {
+      debugger
+      var res = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+      const result = res.test(value)
+      console.log(result);
+      if (!result) {
+        callback(new Error('请输入正确的邮箱地址'))
+      }
+    },
+    validateIdCard(rule, value, callback) {
+      var res = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/
+      const result = res.test(value)
+      console.log(result);
+      if (!result) {
+        callback(new Error('请输入正确的身份证号码'))
+      }
+    },
+    validateMobile(rule, value, callback) {
+      var res = /^[1][3,4,5,6.7,8,9][0-9]{9}$/
+      const result = res.test(value)
+      console.log(result);
+      if (!result) {
+        callback(new Error('请输入正确的手机号码'))
+      }
+    }
+
   }
 };
 </script>
