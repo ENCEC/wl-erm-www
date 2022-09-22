9de5a5388f3f00bd11863f3ac9dae5fa7c3345d9
zuojuntao <2275958074@qq.com>
2022-09-22 10:58:31 +0800

    修改表格高度

--- src/views/my-task/config-data.js
+++ src/views/my-task/config-data.js
@@ -89,8 +89,8 @@ export const tableConfig = {
   highlightCurrentRow: true, // 是否支持当前行高亮显示
   mutiSelect: false, // 是否支持列表项选中功能
   indexShow: true,
-  pagination: true,
-  height: '350px'
+  pagination: true
+  // height: '350px'
 };
 
 // 表格列
